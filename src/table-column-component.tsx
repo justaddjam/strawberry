9bf6cbfae184eae36e99d29f91fd675f55f42d5d
James Richford <jrichford@neworbit.co.uk>
2018-05-25 12:13:58 +0100

    ensure props exported

--- src/table-column-component.tsx
+++ src/table-column-component.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export class TableColumnComponent extends React.Component<TableColumnProps> { }
 
-interface TableColumnProps {
+export interface TableColumnProps {
     getValue: (... args: Array<any>) => any;
     title: string;
 }
